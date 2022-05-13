ef6a0d3421dab65654ef812ed6e608c55822f408
Soun9922 <sagsounak39@gmail.com>
2022-05-13 17:29:21 +0530

    Second Commit is done.

--- src/employeewagecalculator/EmployeeWage.java
+++ src/employeewagecalculator/EmployeeWage.java
@@ -3,12 +3,17 @@ package employeewagecalculator;
 import java.util.Random;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Competition");
-        int attendanceCheck = (int)Math.floor(Math.random() * 10) % 2;
+        int attendanceCheck = (int) Math.floor(Math.random() * 10) % 2;
         if (attendanceCheck == 0)
             System.out.println("Employee is absent.");
-        else
-            System.out.println("Employee is Present.");
+        else {
+            int employeeWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+            System.out.println("Employee is Present, The Wage is : " + employeeWage);
+        }
     }
 }
