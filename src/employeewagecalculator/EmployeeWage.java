a30184416f9179cb243a1c4a7371805fed7dc9d5
Soun9922 <sagsounak39@gmail.com>
2022-05-13 17:54:00 +0530

    Employee Part Time and Wage Calculator.

--- src/employeewagecalculator/EmployeeWage.java
+++ src/employeewagecalculator/EmployeeWage.java
@@ -4,17 +4,22 @@ import java.util.Random;
 
 public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
-    static final int FULL_TIME_HOUR = 8;
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
 
     public static void main(String[] args) {
-        int IsAbsent = 1;
-        System.out.println("Welcome to Employee Wage Competition");
-        int attendanceCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (attendanceCheck == IsAbsent)
-            System.out.println("Employee is absent.");
-        else {
-            int employeeWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-            System.out.println("Employee is Present, The Wage is : " + employeeWage);
-        }
+        System.out.println("----Welcome to Employee Wage Competition----");
+        Random random = new Random();
+        int empAttendance = random.nextInt(2) + 1;
+        if (empAttendance == 1) {
+            int full_time_Salary = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            System.out.println("The Employee Salary : " + full_time_Salary);
+        } else if (empAttendance == 2) {
+            int Part_time_Salary = (WAGE_PER_HOUR * PART_TIME_HOUR);
+            System.out.println("The PartTime Employee Salary : " + Part_time_Salary);
+        } else
+            System.out.println("The Employee is absent.");
     }
 }
+
+
