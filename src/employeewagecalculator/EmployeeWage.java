d8c9339340dea4abc531d86dcdfb4ee35d66e4e5
Soun9922 <sagsounak39@gmail.com>
2022-05-13 18:26:58 +0530

    Calculation of Wages for a month.

--- src/employeewagecalculator/EmployeeWage.java
+++ src/employeewagecalculator/EmployeeWage.java
@@ -3,6 +3,10 @@ package employeewagecalculator;
 import java.util.Random;
 
 public class EmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+    static final int WORKING_DAY_PER_MONTH = 20;
     public static void main(String[] args) {
         System.out.println("----Welcome to Employee Wage Competition----");
         double checkAttendance = Math.round(Math.random());
@@ -11,7 +15,8 @@ public class EmployeeWage {
                 System.out.println("The Employee is Absent.");
                 break;
             default:
-                System.out.println("The Employee is Present.");
+                int totalMonthlySalary = WAGE_PER_HOUR * FULL_DAY_HOUR * WORKING_DAY_PER_MONTH;
+                System.out.println("The Employee is Present, The monthly salary of the Employee : " + totalMonthlySalary);
         }
     }
 }
